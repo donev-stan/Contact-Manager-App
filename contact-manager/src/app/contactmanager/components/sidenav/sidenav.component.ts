86498d3d5d8375de4ee7b928c69c9f1fb8b9501e
Stanislav Donev <77002111+donev-stan@users.noreply.github.com>
2022-09-13 18:00:32 +0430

    sidenav toggle button and responsiveness

--- contact-manager/src/app/contactmanager/components/sidenav/sidenav.component.ts
+++ contact-manager/src/app/contactmanager/components/sidenav/sidenav.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  BreakpointObserver,
+  Breakpoints,
+  BreakpointState,
+} from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-sidenav',
@@ -6,6 +11,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sidenav.component.scss'],
 })
 export class SidenavComponent implements OnInit {
-  constructor() {}
-  ngOnInit(): void {}
+  public isScreenSmall: boolean = false;
+
+  constructor(private breakpointObserver: BreakpointObserver) {}
+
+  ngOnInit(): void {
+    this.breakpointObserver
+      .observe([Breakpoints.XSmall])
+      .subscribe((state: BreakpointState) => {
+        console.log(state);
+
+        this.isScreenSmall = state.matches;
+      });
+  }
 }
--- contact-manager/src/app/shared/material.module.ts
+++ contact-manager/src/app/shared/material.module.ts
@@ -4,10 +4,20 @@ import { CommonModule } from '@angular/common';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
   declarations: [],
   imports: [CommonModule],
-  exports: [MatSidenavModule, MatToolbarModule, MatListModule],
+  exports: [
+    MatSidenavModule,
+    MatToolbarModule,
+    MatListModule,
+    MatIconModule,
+    MatButtonModule,
+    MatCardModule,
+  ],
 })
 export class MaterialModule {}
